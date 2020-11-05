ad95671a5e50caf16f86a851c3370ddb95a24b70
enriquetecfan <enriquemondejar15@gmail.com>
2020-11-05 18:14:53 +0100

    add new style

--- src/index.js
+++ src/index.js
@@ -2,28 +2,31 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
 import "./styles.css";
-import Card from "react-bootstrap/CardColumns";
+import Card from "react-bootstrap/Card";
+import CardColumns from "react-bootstrap/CardColumns";
+import { CardGroup } from "react-bootstrap";
+
 
 function App() {
   const [coins, setCoins] = useState(null);
 
   const fetchData = async () => {
     const response = await axios.get(
-      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&ids=bitcoin,tether,bitcoin-cash,ethereum,monero,Chainlink,litecoin&page=1&sparkline=false"
-    );
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&ids=bitcoin,tether,bitcoin-cash,ethereum,monero,Chainlink,litecoin&order=market_cap_desc&sparkline=false");
 
     setCoins(response.data);
   };
 
   return (
     <div className="App">
-      <h1>Crypto Currency Coins</h1>
-      <h2>Get a list of Crypto Currency Coins</h2>
+      <h1>CryptoCurrency </h1>
+      <h2>Get a list of Crypto Currency in order of market cap descent</h2>
+      <h2>Prices are in Dollars($)</h2>
 
       {/* Fetch data from API */}
       <div>
         <button className="fetch-button" onClick={fetchData}>
-          Get Data
+          Get List
         </button>
         <br />
       </div>
@@ -34,27 +37,19 @@ function App() {
         {coins &&
           coins.map((coins, index) => {
             return (
-            <ul>
-              <li>
-                <img alt="" className="imagen" src={coins.image}/>
-              </li>
-              <li>
-               Coin Name -> {coins.name}
-              </li>
-              <li>
-               Crypto Value -> {coins.current_price} $
-              </li>
-              <li>
-                Price chage in 24 hours -> {coins.price_change_24h} $
-              </li>
-              <li>
-                Price High in 24 hours -> {coins.high_24h} $ 
-               </li>
-               <li> 
-                Price Low in 24 hours -> {coins.low_24h} $
-              </li>
-
-            </ul>
+              <div className="coin">
+                <CardGroup>
+                  <Card style={{ width: "18rem" }}>
+                    <Card.Img variant="top" className="imagen" src={coins.image} />
+                    <Card.Body>
+                      <Card.Title>{coins.id} "{coins.symbol}"</Card.Title>
+                      <Card.Text>Price -> {coins.current_price}$</Card.Text>
+                      <Card.Text>Price Change 24 Hour -> {coins.price_change_24h}$</Card.Text>
+                      <Card.Text>Best price in 24 hours {coins.high_24h}$  and Lowest price in 24 hours {coins.low_24h}$</Card.Text>
+                    </Card.Body>
+                  </Card>
+                </CardGroup>
+              </div>
             );
           })}
       </div>
